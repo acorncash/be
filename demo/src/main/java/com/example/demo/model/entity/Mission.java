f6aa56818084fd589abf50861247c9f8fe049e89
donguk0 <140999966+donguk0@users.noreply.github.com>
2023-10-10 13:56:28 +0900

    유저에게 보여주는 미션 리스트 startAt 조건 limitCnt 건 추가

--- demo/src/main/java/com/example/demo/model/entity/Mission.java
+++ demo/src/main/java/com/example/demo/model/entity/Mission.java
@@ -39,7 +39,9 @@ public class Mission {
 
     private String answer;
 
-    private Integer limitCnt;
+    @NotNull
+    @Builder.Default
+    private Integer limitCnt = 0;
 
     @NotNull
     @Builder.Default
@@ -55,10 +57,12 @@ public class Mission {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateAt = new Date();
 
+    @NotNull
     @Builder.Default
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startAt = new Date();
 
+    @NotNull
     @Builder.Default
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endAt = new Date();
--- demo/src/main/java/com/example/demo/repository/MissionInterface.java
+++ demo/src/main/java/com/example/demo/repository/MissionInterface.java
@@ -2,8 +2,11 @@ package com.example.demo.repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.entity.Mission;
@@ -11,6 +14,11 @@ import com.example.demo.model.entity.Mission;
 @Repository
 public interface MissionInterface extends JpaRepository<Mission, Long> {
     List<Mission> findAlLMissionByDelYn(String delYn);
-    List<Mission> findByMissionTypeAndDelYn(String missionType, String delYn);
+
+//    List<Mission> findByMissionTypeAndDelYn(String missionType, String delYn);
+
     Optional<Mission> findBySeqAndDelYn(Integer seq, String delYn);
+
+    @Query("SELECT m FROM mission m WHERE m.missionType = :missionType AND m.delYn = :delYn AND NOW() BETWEEN m.startAt AND m.endAt AND m.limitCnt > m.attendCnt")
+    List<Mission> findByMissionTypeAndDelYn(@Param("missionType") String missionType, @Param("delYn") String delYn);
 }
