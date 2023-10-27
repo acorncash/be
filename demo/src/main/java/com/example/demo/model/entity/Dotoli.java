2425fe9ded9146935f570384355fb46b21835052
grpel2108 <grpel2108@gmai.com>
2023-10-28 01:46:48 +0900

    Dotoli > Mission FK 제거
    데이터 관리의 편의성을 위해 제거

--- demo/src/main/java/com/example/demo/model/entity/Dotoli.java
+++ demo/src/main/java/com/example/demo/model/entity/Dotoli.java
@@ -1,17 +1,24 @@
 package com.example.demo.model.entity;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -53,10 +60,10 @@ public class Dotoli {
     private String ipAddress;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userSeq", insertable = false, updatable = false)
+    @JoinColumn(name = "userSeq", insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "missionSeq", insertable = false, updatable = false)
+    @JoinColumn(name = "missionSeq", insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Mission mission;
 }
